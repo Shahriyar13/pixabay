8011d07930965aec0c264a6044ac3554af43a777
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-04-02 23:41:38 +0200

    Update PostsScreen.kt

--- app/src/main/java/email/aghajani/pixabay/ui/features/posts/PostsScreen.kt
+++ app/src/main/java/email/aghajani/pixabay/ui/features/posts/PostsScreen.kt
@@ -1,9 +1,6 @@
 package email.aghajani.pixabay.ui.features.posts
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -14,8 +11,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 
 @Composable
 fun PostsScreen(
@@ -29,27 +28,31 @@ fun PostsScreen(
             items = posts.value,
             key = { it.id },
         ) { post ->
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                alignment = Alignment.TopCenter,
+                model = post.imagePreview.url,
+                contentDescription = post.tags.toString(),
+                contentScale = ContentScale.FillWidth,
+            )
             Text(
                 modifier = Modifier.height(75.dp),
                 text = post.user.userName,
             )
             Divider()
         }
-        item {
+        if (viewModel.loading.value) item {
             Column(
-                modifier = Modifier
-                    .fillParentMaxSize(),
+                modifier = if (viewModel.fetchImageParams.page == 1) Modifier.fillParentMaxSize() else Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
             ) {
                 Text(
                     modifier = Modifier.padding(8.dp),
-                    text = "Refresh Loading"
+                    text = "Loading"
                 )
-
                 CircularProgressIndicator(color = Color.Black)
             }
-
         }
     }
 }
